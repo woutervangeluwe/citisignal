2dd78b0707ec0025266cf59f8ad4637a5b92a505
Wouter Van Geluwe <vangeluw@adobe.com>
2025-01-27 13:00:38 +0100

    js css

--- blocks/fiberoffer/fiberoffer.js
+++ blocks/fiberoffer/fiberoffer.js
@@ -1,6 +1,14 @@
 export default function decorate(block) {
     const [fiberOfferWrapper] = block.children;
   
+    offerText = block.children[0];
+    offerCTA = block.children[1];
+
+    offerText.id = 'offerText';
+    offerText.className = 'offerText';
+    offerCTA.id = 'offerCTA';
+    offerCTA.className = 'offerCTA';
+
     const blockFiberOffer = document.createElement('blockFiberOffer');
     blockFiberOffer.textContent = fiberOfferWrapper.textContent.trim();
     fiberOfferWrapper.replaceChildren(blockFiberOffer);
