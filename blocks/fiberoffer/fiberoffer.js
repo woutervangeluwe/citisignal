f7b1f0ca63de3e70d2e857c7fc4fe01f7d43bb6e
Wouter Van Geluwe <vangeluw@adobe.com>
2025-01-27 13:05:46 +0100

    js css

--- blocks/fiberoffer/fiberoffer.js
+++ blocks/fiberoffer/fiberoffer.js
@@ -3,11 +3,14 @@ export default function decorate(block) {
   
     const offerText = block.children[0];
     const offerCTA = block.children[1];
+    const offerImage = block.children[2];
 
     offerText.id = 'offerText';
     offerText.className = 'offerText';
     offerCTA.id = 'offerCTA';
     offerCTA.className = 'offerCTA';
+    offerImage.id = 'offerImage';
+    offerImage.className = 'offerImage';
 
     const blockFiberOffer = document.createElement('blockFiberOffer');
     blockFiberOffer.textContent = fiberOfferWrapper.textContent.trim();
