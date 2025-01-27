9c337bc5a99b56975d5de009f459421a58707e54
Wouter Van Geluwe <vangeluw@adobe.com>
2025-01-27 13:42:03 +0100

    js css

--- blocks/fiberoffer/fiberoffer.js
+++ blocks/fiberoffer/fiberoffer.js
@@ -12,7 +12,9 @@ export default function decorate(block) {
     offerImage.id = 'offerImage';
     offerImage.className = 'offerImage';
 
-    document.querySelector("#offerCTA").innerHTML="test";
+    document.querySelector("#offerText").innerHTML="Text";
+    document.querySelector("#offerCTA").innerHTML="CTA";
+    document.querySelector("#offerImage").innerHTML="<img style='max-width:100%;' src='https://vangeluw.blob.core.windows.net/vangeluw/animal-Wouter-1737058328.png?sv=2023-01-03&st=2025-01-27T12%3A26%3A43Z&se=2025-01-28T12%3A26%3A43Z&sr=b&sp=r&sig=UfyWaphZm2Jx07szWuknI1oBgqe3hJjLwTSsQnLKCxs%3D'/>";;
 
     const blockFiberOffer = document.createElement('blockFiberOffer');
     blockFiberOffer.textContent = fiberOfferWrapper.textContent.trim();
